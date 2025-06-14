044a1cf9372abd5d87431456671b90244b21444c
Sebastian Cespedes <159057371+SebastianCespedes12@users.noreply.github.com>
2025-06-14 15:47:32 -0500

    Add files via upload

--- Prototipo.py
+++ Prototipo.py
@@ -1,4 +1,5 @@
 from random import uniform
+from matplotlib import pyplot
 
 class Particle:
     def __init__(self,posicion: list, funcion, limites: tuple):
@@ -6,7 +7,7 @@ class Particle:
         self.limites = limites
         distribucion_velocidad = uniform(-abs(limites[0] - limites[1]), abs(limites[0] - limites[1]))
         self.velocidad = [distribucion_velocidad, distribucion_velocidad]
-        self.mejor_pos_local = posicion
+        self.mejor_pos_local = posicion.copy()
         self.mejor_val_local = funcion(*self.posicion)
 
     def mover(self):
@@ -16,7 +17,7 @@ class Particle:
                 self.posicion[i] = self.limites[0]
             elif self.posicion[i] > self.limites[1]:
                 self.posicion[i] = self.limites[1]
-
+                
     def buscar_mejor_local(self, funcion):
         mejor_val_local_posible = funcion(*self.posicion) 
         if mejor_val_local_posible < self.mejor_val_local:
@@ -24,14 +25,14 @@ class Particle:
             self.mejor_pos_local = self.posicion.copy()
         return self.mejor_pos_local, self.mejor_val_local
     
-    def cambiar_velocidad(self, parametro_cognitivo: float, parametro_social:float, mejor_pos_global:list):
+    def cambiar_velocidad(self, coeficiente_inercia:float ,parametro_cognitivo: float, parametro_social:float, mejor_pos_global:list):
         nueva_velocidad = []
         for i in range(len(self.velocidad)):
             r1 = uniform(0, 1)
             r2 = uniform(0, 1)
             cognitivo = parametro_cognitivo * r1 * (self.mejor_pos_local[i] - self.posicion[i])
             social = parametro_social * r2 * (mejor_pos_global[i] - self.posicion[i])
-            nueva_velocidad.append(self.velocidad[i] + cognitivo + social)
+            nueva_velocidad.append(coeficiente_inercia * self.velocidad[i] + cognitivo + social)
         self.velocidad = nueva_velocidad
 
 class Swarm:
@@ -39,29 +40,30 @@ class Swarm:
         self.enjambre = enjambre
         self.mejor_pos_global = []
         self.mejor_val_global = float('inf')
-    
+
     def buscar_mejor_global(self, funcion):
         for particula in self.enjambre:
             if particula.buscar_mejor_local(funcion)[1] < self.mejor_val_global:
                 self.mejor_val_global = particula.buscar_mejor_local(funcion)[1]
                 self.mejor_pos_global = particula.buscar_mejor_local(funcion)[0].copy()
-    
-    def cambiar_velocidades(self, parametro_cognitivo, parametro_social):
-        for particula in self.enjambre:
-            particula.cambiar_velocidad(parametro_cognitivo, parametro_social, self.mejor_pos_global)
-            particula.mover()
 
+    def cambiar_velocidades(self, coeficiente_inercia = 0.7, parametro_cognitivo =2, parametro_social = 2):
+        for particula in self.enjambre:
+            particula.cambiar_velocidad(coeficiente_inercia, parametro_cognitivo, parametro_social, self.mejor_pos_global)
+            particula.mover()
+            
     def mejor_global_encontrado(self):
         return self.mejor_val_global, self.mejor_pos_global
         
+
 if __name__ == "__main__":
     def goldstein_price(x, y):
         term1 = 1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)
-        term2 = 30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y** 2)
+        term2 = 30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2)
         return term1 * term2
 
     enjambre = []
-    for i in range(100):
+    for i in range(20):
         posicion_prueba = [uniform(-2.0, 2.0), uniform(-2.0, 2.0)]
         limites_prueba = (-2,2)
         particula = Particle(posicion_prueba, goldstein_price, limites_prueba)
@@ -69,8 +71,16 @@ if __name__ == "__main__":
 
     prueba = Swarm(enjambre)
     
-    for i in range(50):
+    for i in range(201):
         prueba.buscar_mejor_global(goldstein_price)
-        prueba.cambiar_velocidades(parametro_cognitivo= 2 ,parametro_social= 2)
+        if i in [25, 50, 150, 200]:
+            for particula in prueba.enjambre: 
+                pyplot.plot(*particula.posicion, "o")  
+                pyplot.title(f"Iteracion {i}") 
+            pyplot.show()
+        prueba.cambiar_velocidades()
 
-    print(prueba.mejor_global_encontrado())
+    mejor_val_global = prueba.mejor_global_encontrado()[0]
+    mejor_pos_global = prueba.mejor_global_encontrado()[1]
+    print(f"Optimizacion: {mejor_val_global:.3f},\nPosicion:", end=" ")
+    print([f"{i:.3f}" for i in mejor_pos_global])
